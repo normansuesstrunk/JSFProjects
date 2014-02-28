dab71e83d5c74291e6277bc972c2eb8f9509c1a0
n.suesstrunk@gmail.com <n.suesstrunk@gmail.com>
2014-02-28 14:51:05 +0100

    - Renamed the index.html to index.xhtml. Facelets only works if it has the ending ".xhtml" - Added no argument constructor to the User Bean - Adjusted the web.xml

--- JSFStart/src/org/company/User.java
+++ JSFStart/src/org/company/User.java
@@ -1,11 +1,22 @@
 package org.company;
 
+/**
+ * 
+ * @author Norman Suesstrunk
+ */
 public class User {
 	
 	private String name;
 	
+	/**
+	 * a constructor with no arguments is needed 
+	 * when we plainly reference the bean field "name"
+	 */
+	public User() {
+		this.name = "Peter";
+	}
+	
 	public User(String name) {
-		super();
 		this.name = name;
 	}
 
