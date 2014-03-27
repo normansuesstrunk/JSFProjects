c39e602d54fe59de20c7176f93d160c62b1e56c8
Norman Suesstrunk <n.suesstrunk@gmail.com>
2014-03-27 21:46:17 +0100

    working db connection & realm connectioni

--- JEETutMaven/JEETutMaven/create-table-postgres.sql
+++ JEETutMaven/JEETutMaven/create-table-postgres.sql
@@ -1,13 +1,24 @@
 DROP TABLE IF EXISTS users; 
+DROP TABLE IF EXISTS user_roles; 
+
 DROP SEQUENCE user_id_seq; 
 
 CREATE SEQUENCE user_id_seq;
 
 CREATE TABLE users (
     id INTEGER NOT NULL default nextval('user_id_seq'),
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    user_name VARCHAR(20) NOT NULL,
+    user_pass VARCHAR(50) NOT NULL
 );
 
-INSERT INTO users (username, password)
+INSERT INTO users (user_name, user_pass)
     VALUES ('admin', 'password');
+    
+CREATE TABLE user_roles (
+  	user_name         VARCHAR(20) NOT NULL,
+  	role_name         VARCHAR(20) NOT NULL,
+  	primary key (user_name, role_name)
+);
+
+INSERT INTO user_roles (user_name, role_name)
+    VALUES ('admin', 'ADMIN');
